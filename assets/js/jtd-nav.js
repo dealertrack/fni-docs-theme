fdb01bd362a0cc2d3ef389c6d39bdad843ed3245
Ken Hill <kenneth.hill@coxautoinc.com>
2022-01-20 21:18:26 -0500

    Build nav once and use JS for expansion

---
+++ assets/js/jtd-nav.js
@@ -0,0 +1,33 @@
+// Rather than generating a new left-nav for every page, generate a single nav and use JS to expand the current page
+
+(function (jtd) {
+
+function initNavControls() {
+  const siteNav = document.getElementById('site-nav')
+
+  const pageUrl = siteNav.dataset.pageUrl
+  const pageParent = siteNav.dataset.pageParent
+  const pageGrandParent = siteNav.dataset.pageGrandParent
+
+  console.log({pageUrl, pageParent, pageGrandParent})
+
+  const navItems = siteNav.getElementsByClassName('nav-list-item')
+
+  for (const navItem of navItems) {
+    const itemUrl = navItem.dataset.url
+    const itemTitle = navItem.dataset.title
+
+    console.log({itemUrl, itemTitle})
+    
+    if (itemUrl === pageUrl || itemTitle === pageParent || itemTitle == pageGrandParent) {
+      navItem.classList.add('active')
+      navItem.querySelector('.nav-list-link').classList.add('active')
+    }
+  }
+}
+
+jtd.onReady(function(){
+  initNavControls()
+})
+
+})(window.jtd)
---
+++ fni-docs-theme.gemspec
@@ -0,0 +1,23 @@
+# coding: utf-8
+
+Gem::Specification.new do |spec|
+  spec.name          = "fni-docs-theme"
+  spec.version       = "0.2.0"
+  spec.authors       = ["Patrick Marsceill", "Ken Hill"]
+  spec.email         = ["patrick.marsceill@gmail.com", "rubygems@hill.ae"]
+
+  spec.summary       = %q{Fork of just-the-docs}
+  spec.homepage      = "https://github.com/dealertrack/fni-docs-theme"
+  spec.license       = "MIT"
+
+  spec.files         = `git ls-files -z`.split("\x0").select { |f| f.match(%r{^(assets|bin|_layouts|_includes|lib|Rakefile|_sass|LICENSE|README)}i) }
+  spec.executables   << 'just-the-docs'
+
+  spec.add_development_dependency "bundler", "~> 2.3.5"
+  spec.add_runtime_dependency "jekyll", ">= 4.2.0"
+  spec.add_runtime_dependency "jekyll-include-cache", ">= 0.2.1"
+  spec.add_runtime_dependency "rake", ">= 12.3.1", "< 13.1.0"
+  spec.add_runtime_dependency "webrick", ">= 1.7"
+  spec.add_runtime_dependency "digest", ">= 3.0"
+
+end
