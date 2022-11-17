bf4fa55baf18a43d1aea7b8d8f1c5cc79765038e
John Baczkowski <john@webwises.com>
2022-11-17 15:37:27 -0600

    Separate Nav and Breadcrumbs

--- lib/generators/nav-generator.rb
+++ lib/generators/nav-generator.rb
@@ -5,8 +5,12 @@ module NavGenerator
     def generate(site)
       nav = nav_for_parent(site.pages, nil, nil)
       
+      # Breadcrumbs
+      breadcrumb = breadcrumb_for_parent(site.pages, nil, nil)
+      
       # Attach nav data to the default layout
       site.layouts['default'].data['nav'] = nav
+      site.layouts['default'].data['ol'] = breadcrumb
     end
 
     def nav_for_parent(page_list, parentPage, grandParentPage)
@@ -21,15 +25,26 @@ module NavGenerator
         # Attach the child data to the page itself, used to render the footer Table of Contents
         page.data['children'] = children
         
-        # Attach parent/grandparent URLs for rendering breadcrumb
-        page.data['parent_url'] = parentPage&.url
-        page.data['grand_parent_url'] = grandParentPage&.url
-        
         {
           'title' => page.data['title'],
           'url' => page.url,
           'children' => children
         }
+      }
+    end
+
+    def breadcrumb_for_parent(page_list, parentPage, grandParentPage)
+      parentPageTitle = parentPage && parentPage.data['title']
+      pages = page_list
+        .filter { |page| page.data['parent'] == parentPageTitle && page.data['title'] != nil }
+        .sort_by { |page| [page.data['nav_order'] || 999, page.data['title']] }  
+
+      nav = pages.map { |page|
+        children = breadcrumb_for_parent(page_list, page, parentPage)
+        
+        # Attach parent/grandparent URLs for rendering breadcrumb
+        page.data['parent_url'] = parentPage&.url
+        page.data['grand_parent_url'] = grandParentPage&.url
       }
     end
   end
